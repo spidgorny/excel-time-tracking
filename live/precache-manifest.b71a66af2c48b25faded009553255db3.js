31ac8df7e250fdecefd428b32d417041b6813c5a
spidgorny <spidgorny@gmail.com>
2020-03-24 13:06:14 +0000

    Deploying to master from  - eb479eae517090f4e481db33403ecead26cf3f53 🚀

---
+++ live/precache-manifest.b71a66af2c48b25faded009553255db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835e406ea1e05ffbaefd14f46a3afeaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f725a3f9825c12d08e3a",
+    "url": "/static/css/2.38e38cca.chunk.css"
+  },
+  {
+    "revision": "f4062ab0face2f5edafd",
+    "url": "/static/css/main.023076f3.chunk.css"
+  },
+  {
+    "revision": "f725a3f9825c12d08e3a",
+    "url": "/static/js/2.55a70249.chunk.js"
+  },
+  {
+    "revision": "65e7394045ad374c9757da11d1af02f7",
+    "url": "/static/js/2.55a70249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4062ab0face2f5edafd",
+    "url": "/static/js/main.cb1e4d8a.chunk.js"
+  },
+  {
+    "revision": "3fd444dd0f0276b85aa8",
+    "url": "/static/js/runtime-main.dbd48966.js"
+  }
+]);
